c929bee9f0b8fbfcf0086507779199fb61a5930e
Manoel Carvalho <manoelmecat@gmail.com>
2020-01-17 22:29:22 -0300

    Finish

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,34 @@
+import Dev from '../models/Dev'
+import parseStringToArray from '../utils/parseStringToArray'
+
+module.exports = {
+  async index(req, res) {
+    const {
+      latitude,
+      longitude,
+      techs
+    } = req.query
+
+    const techsArray = parseStringToArray(techs)
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000
+        }
+      },
+    })
+
+    return res.json({
+      devs
+    })
+
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,44 +1,14 @@
 import {
   Router
 } from 'express'
-import Dev from './models/Dev'
-import axios from 'axios'
+import DevController from './controllers/DevController'
+import SearchController from './controllers/SearchController'
 
 const routes = Router()
 
-routes.post('/dev', async (req, res) => {
-  const {
-    github_username,
-    techs
-  } = req.body
+routes.post('/dev', DevController.store)
+routes.get('/devs', DevController.index)
 
-  const response = await axios.get(`https://api.github.com/users/${github_username}`)
-
-  const {
-    name = login, avatar_url, bio
-  } = response.data
-
-  const techsArray = techs.split(',').map(tech => tech.trim())
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArray
-  })
-
-  return res.status(200).json(dev)
-
-})
-
-routes.get('/devs', async (req, res) => {
-  const devs = await Dev.find();
-
-  return res.status(200).json({
-    devs
-  })
-
-})
+routes.get('/search', SearchController.index)
 
 module.exports = routes
